a3f4f02d450eedd21cf80182786168954ec8bade
MechelleGG <752429@student.dusd.net>
2023-03-10 09:56:05 -0800

    I finsihed Lab 2
    I finished lab 2 and i am starting the bonus

--- Unit2BasicGamePlayGaldamez/Assets/Scripts/PlayerController.cs
+++ Unit2BasicGamePlayGaldamez/Assets/Scripts/PlayerController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float horizontalInput;
+    public float verticalInput; 
     public float speed = 10.0f;
     public float xRange = 10;
     public GameObject projectilePrefab;
@@ -29,6 +30,8 @@ public class PlayerController : MonoBehaviour
         }
         horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+        verticalInput = Input.GetAxis("Vertical");
+        transform.Translate(Vector3.forward * verticalInput * Time.deltaTime * speed);
 
         if (Input.GetKeyDown(KeyCode.Space))
         {
