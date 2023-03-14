00f6b364fee01052202d74785b613b18892c713e
MechelleGG <752429@student.dusd.net>
2023-03-14 09:55:05 -0700

    I completed the bonus
    I only completed the bonus medium and only the hard bonus is left

--- Unit2BasicGamePlayGaldamez/Assets/Scripts/PlayerController.cs
+++ Unit2BasicGamePlayGaldamez/Assets/Scripts/PlayerController.cs
@@ -9,6 +9,7 @@ public class PlayerController : MonoBehaviour
     public float speed = 10.0f;
     public float xRange = 10;
     public GameObject projectilePrefab;
+    public Transform projectileSpawnPoint;
 
     // Start is called before the first frame update
     void Start()
@@ -36,7 +37,7 @@ public class PlayerController : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Space))
         {
             //Launch a projectile from the player
-            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
+            Instantiate(projectilePrefab, projectileSpawnPoint.position, projectilePrefab.transform.rotation);
         }
     }
 
